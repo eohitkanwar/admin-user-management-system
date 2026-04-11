53b1f4d00f3fa6f75d525782ef173e8ad0a5b7a4
Rohit kanwar <kanwar.rohit2905@gmail.com>
2026-04-11 08:31:22 +0530

    all things update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -39,7 +39,7 @@ router.get('/me', protect, getMe);
 router.put('/change-password', protect, changePassword);
 
 // User history route
-router.getsss('/user-history', protect, adminOnly, getUserActivities );
+router.get('/user-history', protect, adminOnly, getUserActivities );
 
 // GET test endpoint for browser access
 app.get("/test-brevo", async (req, res) => {
